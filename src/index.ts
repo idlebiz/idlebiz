c7cb0b6f2995657dbd6ef122f638cd8deba3ae48
nsylke <nicksylke@outlook.com>
2025-02-26 21:10:31 -0600

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { env } from "@/env";
+import { Client } from "discord.js";
+
+const client = new Client({ intents: [] });
+
+client.on("ready", (readyClient) => {
+  console.log(`Logged in as ${readyClient.user.tag}!`);
+});
+
+(async () => {
+  try {
+    await client.login(env.DISCORD_TOKEN);
+  } catch {
+    await client.destroy();
+  }
+})();
