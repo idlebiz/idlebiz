73cdc92caaaf7ca707c567b8d180de414f9e7217
nsylke <nicksylke@outlook.com>
2025-02-28 20:26:28 -0600

    feat: implement game sync

---
+++ src/game/sync.ts
@@ -0,0 +1,48 @@
+import { env } from "@/env";
+import { mongo } from "@/lib/mongo";
+import type { ChangeStream, ChangeStreamDocument } from "mongodb";
+
+// TODO: fill in collections we wanna watch for changes
+const collections: string[] = [];
+let streams: Record<string, ChangeStream> = {};
+// biome-ignore lint/suspicious/noExplicitAny: temporary
+const resumeTokens: Record<string, any> = {};
+
+export async function sync() {
+  const db = (await mongo()).db(env.MONGO_DATABASE);
+
+  for (const collName of collections) {
+    const coll = db.collection(collName);
+
+    const changeStream = coll.watch([], {
+      resumeAfter: resumeTokens[collName] || undefined,
+    });
+
+    streams[collName] = changeStream;
+
+    changeStream.on("change", (change: ChangeStreamDocument) => {
+      if (change._id) {
+        resumeTokens[collName] = change._id;
+      }
+
+      // TODO: handle change implementation
+    });
+
+    changeStream.on("error", () => {
+      restartSync();
+    });
+  }
+}
+
+export async function restartSync() {
+  await closeSync();
+  setTimeout(sync, 5000);
+}
+
+export async function closeSync() {
+  for (const stream of Object.values(streams)) {
+    await stream.close();
+  }
+
+  streams = {};
+}
